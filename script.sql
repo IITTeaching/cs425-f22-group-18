9be6e394dc2872d50628c4c4f962fcd6609c88a0
udig-v <107013004+udig-v@users.noreply.github.com>
2022-12-11 10:00:36 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,68 +1,121 @@
+drop table if exists Address cascade;
 drop table if exists Branch cascade; 
 drop table if exists Customer cascade; 
-drop table if exists Employee cascade; 
-drop table if exists Loan cascade; 
 drop table if exists Account cascade; 
-drop table if exists Transaction cascade; 
+drop table if exists Employee cascade; 
+drop table if exists Transactions cascade;
+drop table if exists TransferInformation cascade; 
+drop table if exists ExternalInformation cascade; 
+
+
+create table Address
+(
+	address_id 		varchar(10),
+	city 			varchar(20),
+	state 			varchar(20),
+	zip 			char(5),
+	primary key (address_id)
+);
+
 
 create table Branch
 (
-	branch_id	int		UNIQUE,
-	address		varchar(50),
-	primary key (branch_id)
+	branch_id		varchar(15),
+	address_id 		varchar(10),
+	status 			varchar(8) check (Status='Active' or Status='Closed'),
+	primary key (branch_id), 
+	foreign key (address_id) references Address
 );
+
 
 create table Customer
 (
-	customer_id	int		UNIQUE,
-	name		varchar(30)	NOT NULL,
-	address		varchar(50),
-	branch_id	int,
+	customer_id		varchar(15),
+	name			varchar(20),
+	password		varchar(12),
+	branch_id		varchar(15),
+	address_id		varchar(15),
 	primary key (customer_id),
-	foreign key (branch_id) references Branch on delete cascade
+	foreign key (branch_id) references Branch, 
+	foreign key (address_id) references Address
 );
 
-create table Employee 
+
+create table Employee
 (
-	emp_id		int		UNIQUE, 
-	name		varchar(30) NOT NULL,
-	branch_id	int,
-	address		varchar(50), 
-	salary		numeric(10,2), 
-	ssn			int		UNIQUE, 
-	type		varchar(15)		check (type = 'teller' or type = 'loan_specialist' or type = 'manager'),
-	primary key (emp_id, ssn), 
-	foreign key (branch_id) references Branch on delete cascade
+	employee_id 	varchar(15),
+	name 			varchar(50),
+	password 		varchar(12),
+	ssn 			char(9),
+	position 		varchar(9) check (position = 'Manager' or position = 'Teller'),
+	salary 			numeric(11,2),
+	branch_Id 		varchar(15),
+	address_id 		varchar(15),
+	primary key (employee_id),
+	foreign key (address_id) references Address, 
+	foreign key (branch_id) references Branch
 );
 
-create table Loan
-(
-	loan_id		int		UNIQUE, 
-	amount		numeric(10,2),
-	runtime		int,
-	interest_schedule	varchar(50), 
-	customer_id	int,
-	primary key (loan_id),
-	foreign key (customer_id) references Customer on delete cascade
-);
 
 create table Account
 (
-	account_id	int		UNIQUE,
-	type		varchar(20)	check (type = 'checking' or type = 'saving'),
-	balance		numeric(10,2),
-	customer_id	int,
+	account_id 		varchar(15),
+	balance 		numeric(11,2),
+	type 			varchar(10) check (type = 'Saving' or type = 'Checking'),
+	customer_id 	varchar(15),
+	status 			varchar(8) check (status = 'Active' or Status = 'Closed'),
 	primary key (account_id),
-	foreign key (customer_id) references Customer on delete cascade
+	foreign key (customer_id) references Customer
+	
 );
 
-create table Transaction
+
+create table Transactions
 (
-	transaction_id	int	UNIQUE, 
-	description	varchar(300),
-	amount		numeric(10,2) NOT NULL,
-	type		varchar(20)		check (type = 'deposit' or type = 'withdrawal' or type = 'transfer'),
-	account_id	int,
-	primary key (transaction_id),
-	foreign key (account_id) references Account on delete cascade
+	transaction_id 	varchar(15),
+	amount 			numeric(11,2),
+	description 	varchar(100),
+	type 			varchar(20) check (Type = 'Deposit' or Type = 'Withdrawl' or Type = 'Transfer' or Type = 'ExternalTransfer'),
+	customer_id 	varchar(15),
+	employee_id 	varchar(15),
+	balance_from 	numeric(11,2),
+	balance_to 		numeric(11,2) default NULL,
+	den				date default current_timestamp,
+	primary key (transaction_id)
 );
+
+
+create table TransferInformation
+(
+	transaction_id	varchar(15),
+	from_account 	varchar(15) references Account(Account_Id),
+	to_account 		varchar(15) references Account(Account_Id),
+	primary key (transaction_id, from_account, to_account),
+	foreign key (transaction_id) references Transactions
+);
+
+
+create table ExternalInformation
+(
+	transaction_id 	varchar(15),
+	from_account 	varchar(15) references Account(Account_Id),
+	bank 			varchar(20),
+	account_number 	char(12),
+	routing_number 	char(9),
+	primary key (transaction_id, from_account, bank, account_number, routing_number),
+	foreign key (transaction_id) references Transactions
+);
+
+
+insert into Address values ('1', 'Chicago', 'IL', '60616');
+insert into Address values ('2', 'San-Francisco', 'CA', '31415');
+insert into Address values ('3', 'New York', 'NY', '12011');
+
+insert into Branch values ('1', '1', 'Active');
+insert into Branch values ('2', '2', 'Active');
+insert into Branch values ('3', '3', 'Active');
+
+insert into Customer values ('1', 'Stas', '1234567890', '1', '1');
+
+insert into Employee values ('1', 'John Doe', '1234567890', '123456789', 'Manager', 130000.00, '1', '1');
+insert into Employee values ('2', 'Jane Doe', '0987654321', '987654321', 'Teller', 45000.00, '2', '2');
